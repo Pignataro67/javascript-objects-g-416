3d029691f1a9d1335c3dfdd968408e52818dacac
Pat  Pignataro <pat6284@msn.com>
2019-05-08 16:29:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,18 +1,3 @@
-// var playlist = {
-//   PearlJam: 'Evenflow',
-//   AliceInChains: 'Man in the Box'
-// };
-
-// function updatePlaylist(playlist, artist, song) {
-//   playlist[artist] = song
-//   return playlist
-// }
-
-// function removeFromPlaylist(playlist, artist) {
-//   delete playlist[artist]
-//   return playlist
-// }
-
 var playlist = {
   'Jack Johnson': 'Banana Pancakes',
 };
