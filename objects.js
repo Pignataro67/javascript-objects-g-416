4806ce93d606eab577ea11e4f9c6719e2c16ad31
Pat  Pignataro <pat6284@msn.com>
2019-05-08 16:21:49 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,14 +1,31 @@
+// var playlist = {
+//   PearlJam: 'Evenflow',
+//   AliceInChains: 'Man in the Box'
+// };
+
+// function updatePlaylist(playlist, artist, song) {
+//   playlist[artist] = song
+//   return playlist
+// }
+
+// function removeFromPlaylist(playlist, artist) {
+//   delete playlist[artist]
+//   return playlist
+// }
+
 var playlist = {
-  PearlJam: 'Evenflow',
-  AliceInChains: 'Man in the Box'
-};
+  Nirvana: "Lithium",
+  Sublime: "What I got"
+}
 
 function updatePlaylist(playlist, artist, song) {
   playlist[artist] = song
+
   return playlist
 }
 
 function removeFromPlaylist(playlist, artist) {
   delete playlist[artist]
+
   return playlist
 }
